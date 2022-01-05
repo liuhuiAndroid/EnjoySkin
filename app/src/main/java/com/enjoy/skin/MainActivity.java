b8ba768a33649d17425c1a4f813b5e068ab93321
lh <im.lh@hotmail.com>
2022-01-05 15:56:11 +0800

    skin replay again

--- app/src/main/java/com/enjoy/skin/MainActivity.java
+++ app/src/main/java/com/enjoy/skin/MainActivity.java
@@ -1,27 +1,17 @@
 package com.enjoy.skin;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.res.AssetManager;
-import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.AttributeSet;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.enjoy.skin.fragment.MusicFragment;
 import com.enjoy.skin.fragment.RadioFragment;
 import com.enjoy.skin.fragment.VideoFragment;
 import com.enjoy.skin.widget.MyTabLayout;
 
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,9 +22,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //控件的收集在这里完成
         setContentView(R.layout.activity_main);
-
         View view = findViewById(R.id.test);
-
         MyTabLayout tabLayout = findViewById(R.id.tabLayout);
         ViewPager viewPager = findViewById(R.id.viewPager);
         List<Fragment> list = new ArrayList<>();
@@ -49,9 +37,7 @@ public class MainActivity extends AppCompatActivity {
                 (getSupportFragmentManager(), list, listTitle);
         viewPager.setAdapter(myFragmentPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
-
     }
-
 
     /**
      * 进入换肤
